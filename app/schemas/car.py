2ad781eb7e9ffbb25c5429f080df146e9ca7b301
rafbiii <rafiqlabib0515@gmail.com>
2026-05-11 20:14:14 +0700

    updated car schema

--- app/schemas/car.py
+++ app/schemas/car.py
@@ -39,11 +39,6 @@ class CarListResponse(BaseModel):
     brand: str
     model: str
     price: float
-    year: int
-    transmission: str
-    mileage: int
-    fuel: str
-    car_type: str
     status: str
     condition: str
     thumbnail_url: str
@@ -55,11 +50,6 @@ class CarListResponse(BaseModel):
             brand=c.brand,
             model=c.model,
             price=c.price,
-            year=c.year,
-            transmission=c.transmission,
-            mileage=c.mileage,
-            fuel=c.fuel,
-            car_type=c.car_type,
             status=c.status,
             condition=c.condition,
             thumbnail_url=c.images[0] if c.images else ""
